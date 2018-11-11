a1638a1f411bc2676dd6c70c1b0dc1a94f790543
kmosakowska <kng.mosak@gmail.com>
2018-11-11 18:05:51 +0100

    Fixed formatting

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,26 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		hello("World");
+    public static void main(String[] args) {
+        hello("World");
 
-		double len = 5;
+        double len = 5;
         System.out.println("Area of square with edge lenght " + len + " = " + area(len));
 
         double a = 4;
         double b = 6;
         System.out.println("Area of rectangle with edges lenght " + a + " and " + b + " = " + area(a, b));
-	}
+    }
 
-	public static void hello(String somebody) {
+    public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
 
-    public static double area (double l) {
-	    return l * l;
+    public static double area(double l) {
+        return l * l;
     }
 
     public static double area(double a, double b) {
-	    return a * b;
+        return a * b;
     }
 }
