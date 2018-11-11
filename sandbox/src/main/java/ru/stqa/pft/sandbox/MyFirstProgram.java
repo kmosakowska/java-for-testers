a7fb5ce85ab73108936643e66d032c51aac2257c
kmosakowska <kng.mosak@gmail.com>
2018-11-11 18:04:39 +0100

    Added few practice functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("World");
+
+		double len = 5;
+        System.out.println("Area of square with edge lenght " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Area of rectangle with edges lenght " + a + " and " + b + " = " + area(a, b));
 	}
+
+	public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area (double l) {
+	    return l * l;
+    }
+
+    public static double area(double a, double b) {
+	    return a * b;
+    }
 }
