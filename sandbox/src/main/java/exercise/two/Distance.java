ff356466b969ca8110dff132d214d2f885f49f5e
kmosakowska <kng.mosak@gmail.com>
2018-11-12 13:21:14 +0100

    Ex2 Calculating distance between two points

---
+++ sandbox/src/main/java/exercise/two/Distance.java
@@ -0,0 +1,13 @@
+package exercise.two;
+
+public class Distance {
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point(2, 5);
+		Point p2 = new Point(5, 9);
+
+		System.out.println("Distance between points p1 and p2 equals to: " + Point.distance(p1, p2));
+	}
+
+}
