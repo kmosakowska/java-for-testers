b1315495c9638d960d0bc02c2e8bd07f4aba2241
kmosakowska <kng.mosak@gmail.com>
2018-11-27 19:40:56 +0100

    Added deletion cases and adjusted contact modification test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactDeletionTests extends TestBase {
 
 	@Test
 	public void testContactDeletion() {
+		if (!app.getContactHelper().isThereAContact()) {
+			app.getNavigationHelper().goToGroupPage();
+			if (! app.getGroupHelper().isThereAGroup()) {
+				app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+			}
+			app.getNavigationHelper().goToHomePage();
+			app.getContactHelper().createContact(new ContactData("Name", "Middle", "Last", "Nickname", "Mrs", "Inc", "Test", "test1"), true);
+			app.getNavigationHelper().goToHomePage();
+		}
 		app.getContactHelper().selectContact();
 		app.getContactHelper().deleteSelectedContact();
 		app.getContactHelper().acceptContactDeletion();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,12 +2,18 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
 	@Test
 	public void testContactModification() {
-		if (! app.getContactHelper().isThereAContact()) {
+		app.getNavigationHelper().goToGroupPage();
+		if (! app.getGroupHelper().isThereAGroup()) {
+			app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+		}
+		app.getNavigationHelper().goToHomePage();
+		if (!app.getContactHelper().isThereAContact()) {
 			app.getContactHelper().createContact(new ContactData("Name", "Middle", "Last", "Nickname", "Mrs", "Inc", "Test", "test1"), true);
 			app.getNavigationHelper().goToHomePage();
 		}
